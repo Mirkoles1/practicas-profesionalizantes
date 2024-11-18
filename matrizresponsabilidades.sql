4979577d5ba48e8004ee479bff2119d8e51e7953
Emita314 <emanuel.mamani.quinones2006@gmail.com>
2024-11-18 20:29:36 -0300

    Se sube el archivo sql nuevo

--- matrizresponsabilidades.sql
+++ matrizresponsabilidades.sql
@@ -27,10 +27,11 @@ CREATE TABLE `actividad` (
   `nombre_actividad` varchar(100) NOT NULL,
   `descripcion` text,
   `id_proyecto` int DEFAULT NULL,
+  `estadoActividad` enum('Pendiente','En Progreso','Completada') NOT NULL DEFAULT 'Pendiente',
   PRIMARY KEY (`id_actividad`),
   KEY `id_proyecto` (`id_proyecto`),
   CONSTRAINT `actividad_ibfk_1` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +40,7 @@ CREATE TABLE `actividad` (
 
 LOCK TABLES `actividad` WRITE;
 /*!40000 ALTER TABLE `actividad` DISABLE KEYS */;
+INSERT INTO `actividad` VALUES (1,'Holaa','Panita',4,'Pendiente'),(2,'Petoys','gagaga',4,'Pendiente'),(3,'Hola','jajajja',4,'Pendiente');
 /*!40000 ALTER TABLE `actividad` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,9 +55,7 @@ CREATE TABLE `asignacion` (
   `id_asignacion` int NOT NULL AUTO_INCREMENT,
   `id_actividad` int DEFAULT NULL,
   `fecha_asignacion` date NOT NULL,
-  `estado` enum('Pendiente','En Progreso','Completada') NOT NULL,
   `id_usuario` int DEFAULT NULL,
-  `comentario` text,
   PRIMARY KEY (`id_asignacion`),
   KEY `id_actividad` (`id_actividad`),
   KEY `fk_asignacion_usuario` (`id_usuario`),
@@ -74,6 +74,63 @@ LOCK TABLES `asignacion` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `comentarioavance`
+--
+
+DROP TABLE IF EXISTS `comentarioavance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comentarioavance` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `tiempo_publicacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_actividad` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_actividad` (`id_actividad`),
+  CONSTRAINT `comentarioavance_ibfk_1` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id_actividad`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentarioavance`
+--
+
+LOCK TABLES `comentarioavance` WRITE;
+/*!40000 ALTER TABLE `comentarioavance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentarioavance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notas`
+--
+
+DROP TABLE IF EXISTS `notas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notas` (
+  `id_nota` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `estadoProyecto` enum('Pendiente','En Progreso','Completada') NOT NULL DEFAULT 'Pendiente',
+  `id_proyecto` int NOT NULL,
+  PRIMARY KEY (`id_nota`),
+  KEY `id_proyecto` (`id_proyecto`),
+  CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notas`
+--
+
+LOCK TABLES `notas` WRITE;
+/*!40000 ALTER TABLE `notas` DISABLE KEYS */;
+INSERT INTO `notas` VALUES (1,'Por favor Funciona','JAJAJJAJA\n','Pendiente',4);
+/*!40000 ALTER TABLE `notas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `proyecto`
 --
 
@@ -83,12 +140,10 @@ DROP TABLE IF EXISTS `proyecto`;
 CREATE TABLE `proyecto` (
   `id_proyecto` int NOT NULL AUTO_INCREMENT,
   `nombre_proyecto` varchar(100) NOT NULL,
-  `fecha_inicio` date DEFAULT NULL,
-  `fecha_fin` date DEFAULT NULL,
   `descripcion` text,
   `estado` enum('Pendiente','En Progreso','Completado') NOT NULL DEFAULT 'Pendiente',
   PRIMARY KEY (`id_proyecto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,6 +152,7 @@ CREATE TABLE `proyecto` (
 
 LOCK TABLES `proyecto` WRITE;
 /*!40000 ALTER TABLE `proyecto` DISABLE KEYS */;
+INSERT INTO `proyecto` VALUES (4,'Final','Ponyta','Pendiente');
 /*!40000 ALTER TABLE `proyecto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +171,7 @@ CREATE TABLE `usuario` (
   `rol` enum('Administrador','Empleado') NOT NULL,
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,6 +180,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Emanuel','kraken0406tp@gmail.com','$2b$10$RbfemFAHWkVkhWnqKNqT2OaAYilxoeVYDrdmiDLP1munxVIyV9cay','Administrador'),(2,'Pepitos','emanuel.mamani.quinones2006@gmail.com','$2b$10$jU31KwSi7rsuwTznQKs3.O55qOiaCepfjlCppYCk/QsKEilyae.gC','Empleado');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +200,7 @@ CREATE TABLE `usuario_proyecto` (
   KEY `usuario_proyecto_ibfk_1` (`id_usuario`),
   CONSTRAINT `usuario_proyecto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `usuario_proyecto_ibfk_2` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,6 +209,7 @@ CREATE TABLE `usuario_proyecto` (
 
 LOCK TABLES `usuario_proyecto` WRITE;
 /*!40000 ALTER TABLE `usuario_proyecto` DISABLE KEYS */;
+INSERT INTO `usuario_proyecto` VALUES (4,1,4);
 /*!40000 ALTER TABLE `usuario_proyecto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -164,4 +222,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-15  9:18:22
+-- Dump completed on 2024-11-18 20:28:35
